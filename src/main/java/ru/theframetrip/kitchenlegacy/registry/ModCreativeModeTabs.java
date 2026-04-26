8a42e882de55bdef4f55cd8cb0ba4f951b2e6d53
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-04-27 00:46:45 +0300

    Create ModCreativeModeTabs.java

---
+++ src/main/java/ru/theframetrip/kitchenlegacy/registry/ModCreativeModeTabs.java
@@ -0,0 +1,63 @@
+package ru.theframetrip.kitchenlegacy.registry;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.RegistryObject;
+import ru.theframetrip.kitchenlegacy.KitchenLegacyMod;
+
+public class ModCreativeModeTabs {
+
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
+            DeferredRegister.create(Registries.CREATIVE_MODE_TAB, KitchenLegacyMod.MOD_ID);
+
+    public static final RegistryObject<CreativeModeTab> KITCHEN_LEGACY_TAB = CREATIVE_MODE_TABS.register("kitchenlegacy",
+            () -> CreativeModeTab.builder()
+                    .title(Component.translatable("itemGroup.kitchenlegacy"))
+                    .icon(() -> new ItemStack(ModItems.TOMATO.get()))
+                    .displayItems((parameters, output) -> {
+                        output.accept(ModItems.JERUSALEM_ARTICHOKE.get());
+                        output.accept(ModItems.BLACK_RADISH.get());
+                        output.accept(ModItems.TURNIP.get());
+                        output.accept(ModItems.ONION.get());
+                        output.accept(ModItems.GARLIC.get());
+                        output.accept(ModItems.CABBAGE.get());
+                        output.accept(ModItems.BROCCOLI.get());
+                        output.accept(ModItems.CAULIFLOWER.get());
+                        output.accept(ModItems.CUCUMBER.get());
+                        output.accept(ModItems.TOMATO.get());
+                        output.accept(ModItems.SWEET_BELL_PEPPER.get());
+                        output.accept(ModItems.HOT_CHILI_PEPPER.get());
+                        output.accept(ModItems.EGGPLANT.get());
+                        output.accept(ModItems.ZUCCHINI.get());
+                        output.accept(ModItems.PATTYPAN_SQUASH.get());
+                        output.accept(ModItems.RADISH.get());
+                        output.accept(ModItems.WHITE_TURNIP.get());
+                        output.accept(ModItems.SWEET_POTATO.get());
+                        output.accept(ModItems.PARSNIP.get());
+                        output.accept(ModItems.RUTABAGA.get());
+                        output.accept(ModItems.DAIKON_RADISH.get());
+                        output.accept(ModItems.PEA_POD.get());
+                        output.accept(ModItems.GREEN_BEANS.get());
+                        output.accept(ModItems.LENTILS.get());
+                        output.accept(ModItems.CELERY.get());
+                        output.accept(ModItems.ASPARAGUS.get());
+                        output.accept(ModItems.ARTICHOKE.get());
+                        output.accept(ModItems.SPINACH.get());
+                        output.accept(ModItems.LETTUCE.get());
+                        output.accept(ModItems.GREEN_ONION.get());
+                        output.accept(ModItems.LEEK.get());
+                        output.accept(ModItems.GINGER_ROOT.get());
+                        output.accept(ModItems.HORSERADISH_ROOT.get());
+                        output.accept(ModItems.RHUBARB.get());
+                        output.accept(ModItems.SEAWEED_KELP.get());
+                    })
+                    .build());
+
+    public static void register(IEventBus eventBus) {
+        CREATIVE_MODE_TABS.register(eventBus);
+    }
+}
