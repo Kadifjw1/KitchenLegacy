21af65d0bcd24b6315cab0d008c0cf84d0e7865d
Kadifjw1 <148907161+Kadifjw1@users.noreply.github.com>
2026-04-27 00:45:58 +0300

    Update KitchenLegacyMod.java

--- src/main/java/ru/theframetrip/kitchenlegacy/KitchenLegacyMod.java
+++ src/main/java/ru/theframetrip/kitchenlegacy/KitchenLegacyMod.java
@@ -1,8 +1,12 @@
 package ru.theframetrip.kitchenlegacy;
 
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+import ru.theframetrip.kitchenlegacy.registry.ModCreativeModeTabs;
+import ru.theframetrip.kitchenlegacy.registry.ModItems;
 
 @Mod(KitchenLegacyMod.MOD_ID)
 public class KitchenLegacyMod {
@@ -11,6 +15,11 @@ public class KitchenLegacyMod {
     public static final Logger LOGGER = LogUtils.getLogger();
 
     public KitchenLegacyMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
+        ModCreativeModeTabs.register(modEventBus);
+
         LOGGER.info("Наследие кухни загружается...");
     }
 }
